0a5de0adc5bceee1892086ea7d97b74138010359
GitiHubi <marco.schreyer@unisg.ch>
2020-08-10 21:25:30 +0200

    added code comments and todos

--- selfsup/methods/train.py
+++ selfsup/methods/train.py
@@ -4,22 +4,35 @@ import os
 import importlib
 from selfsup.utils.generic import load_config
 
+# define main function
 def main(args):
-    # Load config
+
+    # load yaml config file
     config = load_config(os.path.join(args.config_path, "config.yml"))
     
-    # Get method trainer dynamically
+    # get method trainer dynamically based on selected method
     method_trainer = importlib.import_module(f"selfsup.methods.{config['method']}.trainer")
+
+    # get trainer class corresponding to method
     Trainer = getattr(method_trainer, "Trainer")
+
+    # initialize trainer class
     trainer = Trainer(config)
 
-    # Start training
+    # start training
     trainer.train()
 
 
 if __name__ == "__main__":
+
+    # init argument parser
     parser = argparse.ArgumentParser()
+
+    # add yaml config path
     parser.add_argument("--config_path", type=str, required=True)
+
+    # parse arguments
     args = parser.parse_args()
 
+    # call main function
     main(args)
